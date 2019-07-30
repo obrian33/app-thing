d0b07eccd001a20a012d08ff1289e69a4560b9a3
O'Brian Santos <obrians@uci.edu>
2019-07-29 21:53:24 -0700

    Some basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+export function NavBar() {
+  return (
+    <div className="row p-2">
+      <div className="col">
+        THING
+      </div>
+    </div>
+  );
+}
+
+export function FormComponent() {
+  const formStuff = [
+    <div className="form-group col">
+      <label for="name">Name</label>
+      <input type="name" className="form-control" placeholder="Name"></input>
+    </div>
+  ];
+  return (
+    <form>
+      <div className="form-row">
+        {formStuff.map(x => x)}
+        {formStuff.map(x => x)}
+        {formStuff.map(x => x)}
+      </div>
+      <div className="form-row">
+        {formStuff.map(x => x)}
+        {formStuff.map(x => x)}
+        {formStuff.map(x => x)}
+      </div>
+      <div className="form-row">
+        {formStuff.map(x => x)}
+        {formStuff.map(x => x)}
+        {formStuff.map(x => x)}
+      </div>
+      <button type="button" className="btn btn-secondary">Other</button>
+      <button type="button" className="btn btn-success float-right">Sign in</button>
+    </form>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid" style={{ height: "100vh" }}>
+      <NavBar></NavBar>
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-header bg-info">
+              Thing 2
+            </div>
+            <div className="card-body">
+              <FormComponent></FormComponent>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
